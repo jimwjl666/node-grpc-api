7ad2d4dd4e3a17d9266274299e3b5285fcbb3466
wujiaolong <wujiaolong@tron.network>
2018-06-23 12:25:40 +0800

    添加转账接口demo

---
+++ README.md
@@ -0,0 +1,8 @@
+# 安装
+npm install
+
+# 运行
+node server.js  进行接口测试
+
+#说明
+//其它方法在 src > grpc.js中，需要添加可以仿照着继续写
---
+++ test/client.js
@@ -0,0 +1,36 @@
+const {bytesToString} = require("../src/utils/bytes");
+const {GrpcClient} = require("../src");
+const assert = require("assert");
+const getContractListFromTransaction = require("../src/lib/code").getContractListFromTransaction;
+
+
+describe('client', () => {
+
+  let client = new GrpcClient({
+    hostname: "47.254.146.147",
+    port: 50051,
+  });
+
+  it('list nodes', async () => {
+    let nodes = await client.getNodes();
+    for (let node of nodes) {
+      console.log("node", node.getAddress().getHost_asB64())
+    }
+  });
+
+  it('list witnesses', async () => {
+    let witnesses = await client.getWitnesses();
+
+    for (let witness of witnesses) {
+      console.log("witness", witness.getUrl());
+    }
+  });
+
+
+  it('retrieve block by number', async () => {
+    let block = await client.getBlockByNumber(105);
+    console.log("number", block.getBlockHeader().getRawData().getNumber());
+    let contracts = getContractListFromTransaction(block.getTransactionsList()[0]);
+    console.log("number of contracts", contracts.length);
+  });
+});
